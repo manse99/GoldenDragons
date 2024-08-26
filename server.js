479ff0e60552e9d57d4761a1d8e45a02d958684f
GC <40325629+manse99@users.noreply.github.com>
2024-08-26 22:49:57 +0000

    added some functionality to server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
 
 // Constants
 const PORT = 3000;
@@ -13,9 +15,43 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(morgan('combined'));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
 app.get('/', (req, res) => {
-	res.send('Hello remote world!\n');
+    res.send('Hello remote world!\n');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.get('/greet/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello, ${name}!\n`);
+});
+
+app.post('/data', (req, res) => {
+    const data = req.body;
+    res.json({
+        message: 'Data received',
+        data: data
+    });
+});
+
+app.get('/health', (req, res) => {
+    res.send('OK\n');
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start server
+app.listen(PORT, HOST, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
+});
